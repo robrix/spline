204054a2995e7d81608e335358a6cfe9a6dc7d06
Rob Rix <rob.rix@me.com>
2017-03-04 16:04:08 -0500

    Add the size attribute.

--- src/SVG/Drawing.hs
+++ src/SVG/Drawing.hs
@@ -4,6 +4,7 @@ module SVG.Drawing where
 import Control.Monad.Free.Freer
 import Linear.V2 as Linear
 import Text.Blaze.Svg11 as S
+import Text.Blaze.Svg11.Attributes as S
 import Text.Blaze.Svg.Renderer.Pretty as S
 
 data DrawingF a where
@@ -12,6 +13,6 @@ data DrawingF a where
 type Drawing = Freer DrawingF
 
 runDrawing :: Real a => Linear.V2 a -> Drawing () -> String
-runDrawing size = renderSvg . docTypeSvg . iterFreer algebra . fmap (const mempty)
+runDrawing (V2 w h) = renderSvg . (docTypeSvg ! width (stringValue (show (toRational w))) ! height (stringValue (show (toRational h)))) . iterFreer algebra . fmap (const mempty)
   where algebra :: DrawingF x -> (x -> Svg) -> Svg
         algebra drawing cont = mempty
