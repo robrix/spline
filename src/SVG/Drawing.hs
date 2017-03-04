bc31d4feaecbc65e1753fd7fe55c1fcf796eb61a
Rob Rix <rob.rix@me.com>
2017-03-04 16:10:34 -0500

    Parameterize drawings by the numeric type.

--- src/SVG/Drawing.hs
+++ src/SVG/Drawing.hs
@@ -7,22 +7,22 @@ import Text.Blaze.Svg11 as S
 import Text.Blaze.Svg11.Attributes as S
 import Text.Blaze.Svg.Renderer.Pretty as S
 
-data DrawingF a where
-  Move :: Real a => Linear.V2 a -> DrawingF ()
+data DrawingF a f where
+  Move :: Linear.V2 a -> DrawingF a ()
 
-type Drawing = Freer DrawingF
+type Drawing a = Freer (DrawingF a)
 
 -- Smart constructors
 
-move :: Real a => V2 a -> Drawing ()
+move :: Real a => V2 a -> Drawing a ()
 move p = Move p `Then` return
 
 
 -- Running
 
-runDrawing :: Real a => Linear.V2 a -> Drawing () -> String
+runDrawing :: Real a => Linear.V2 a -> Drawing a () -> String
 runDrawing (V2 w h) = renderSvg . (docTypeSvg ! width (realValue w) ! height (realValue h)) . iterFreer algebra . fmap (const mempty)
-  where algebra :: DrawingF x -> (x -> Svg) -> Svg
+  where algebra :: DrawingF a x -> (x -> Svg) -> Svg
         algebra drawing cont = mempty
 
         realValue = stringValue . show . round . toRational
