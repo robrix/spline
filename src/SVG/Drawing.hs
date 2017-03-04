9b892aadbc4277cd7fc39e1a367a7a48edf34aeb
Rob Rix <rob.rix@me.com>
2017-03-04 15:56:15 -0500

    Stub in a way of running a drawing.

--- src/SVG/Drawing.hs
+++ src/SVG/Drawing.hs
@@ -5,6 +5,11 @@ import Control.Monad.Free.Freer
 import Linear.V2 as Linear
 
 data DrawingF a where
-  Move :: Num a => Linear.V2 a -> DrawingF ()
+  Move :: Real a => Linear.V2 a -> DrawingF ()
 
 type Drawing = Freer DrawingF
+
+runDrawing :: Real a => Linear.V2 a -> Drawing () -> ShowS
+runDrawing size = iterFreer algebra . fmap (const id)
+  where algebra :: DrawingF x -> (x -> ShowS) -> ShowS
+        algebra drawing cont = id
