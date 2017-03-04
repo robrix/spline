55e523559c82fbcf3bf8d674104382bd852e35d6
Rob Rix <rob.rix@me.com>
2017-03-04 16:27:46 -0500

    Pull the path algebra into a separate binding.

--- src/SVG/Drawing.hs
+++ src/SVG/Drawing.hs
@@ -32,6 +32,9 @@ runDrawing :: Real a => Linear.V2 a -> Drawing a () -> String
 runDrawing (V2 w h) = S.renderSvg . (S.docTypeSvg ! A.width (realValue w) ! A.height (realValue h)) . iterFreer algebra . fmap (const mempty)
   where algebra :: DrawingF a x -> (x -> S.Svg) -> S.Svg
         algebra drawing cont = case drawing of
-          Path p -> S.path ! A.d (iterFreer (const mempty) (mempty <$ p))
+          Path p -> S.path ! A.d (iterFreer renderPath (mempty <$ p))
+
+        renderPath :: PathF a x -> (x -> S.AttributeValue) -> S.AttributeValue
+        renderPath path cont = mempty
 
         realValue = S.stringValue . show . round . toRational
