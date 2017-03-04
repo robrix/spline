24c9977d8ee7afa6732ef2d80e4a11e873fa9490
Rob Rix <rob.rix@me.com>
2017-03-04 15:52:52 -0500

    Add a type synonym for drawings.

--- src/SVG/Drawing.hs
+++ src/SVG/Drawing.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE GADTs #-}
 module SVG.Drawing where
 
+import Control.Monad.Free.Freer
 import Linear.V2 as Linear
 
 data DrawingF a where
   Move :: Num a => Linear.V2 a -> DrawingF ()
+
+type Drawing = Freer DrawingF
