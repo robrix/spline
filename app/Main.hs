37a1a601a869bd48fb84d8235e74bc848b74a8fb
Rob Rix <rob.rix@me.com>
2017-03-05 20:33:57 -0500

    Define a function to convert polar coordinates to cartesian.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Linear.V2
+import Linear.V2 hiding (angle)
 import Spline.Drawing
 
 main :: IO ()
@@ -11,3 +12,7 @@ main = putStrLn $ runDrawing (V2 200 200) $ do
     move (V2 10 10)
     line (V2 20 20)
     cubic (V2 20 40) (V2 30 30) (V2 50 10)
+
+
+polarToCartesian :: Floating a => a -> a -> V2 a
+polarToCartesian r theta = V2 (r * cos theta) (r * sin theta)
