cbcac345e5534cda414f50fdaa43486c39c3dc16
Rob Rix <rob.rix@me.com>
2017-03-18 20:24:35 -0400

    Go back to multiple paths.

--- app/Main.hs
+++ app/Main.hs
@@ -13,9 +13,10 @@ main = do
   putStrLn $ runDrawing Nothing $ do
     stroke Black
     fill Transparent
-    path . snd $ foldr (\ walk (V2 dx dy, rest) -> (,) (V2 (dx + ddx) (dy + ddy)) $ do
-      move (P (V2 (100 + dx) (100 + dy)))
-      runWalk walk
+    snd $ foldr (\ walk (V2 dx dy, rest) -> (,) (V2 (dx + ddx) (dy + ddy)) $ do
+      path $ do
+        move (P (V2 (100 + dx) (100 + dy)))
+        runWalk walk
       rest) (V2 0 0, return ()) walks
   where V2 ddx ddy = V2 10 0
 
