20cc16348a1285ba73376931c4eddd48787f4d57
Rob Rix <rob.rix@me.com>
2017-03-18 20:16:47 -0400

    Shift each subpath over a bit.

--- app/Main.hs
+++ app/Main.hs
@@ -13,10 +13,11 @@ main = do
   putStrLn $ runDrawing Nothing $ do
     stroke Black
     fill Transparent
-    path $ foldr (\ walk rest -> do
-      move (P (V2 100 100))
+    path . snd $ foldr (\ walk (V2 dx dy, rest) -> (,) (V2 (dx + ddx) (dy + ddy)) $ do
+      move (P (V2 (100 + dx) (100 + dy)))
       runWalk walk
-      rest) (return ()) walks
+      rest) (V2 0 0, return ()) walks
+  where V2 ddx ddy = V2 10 0
 
 
 angle :: Distribution Float
